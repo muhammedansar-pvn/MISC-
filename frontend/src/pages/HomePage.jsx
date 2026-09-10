39307cd1b3ff88ef635a0f66fe9a8104568652b3
muhammed ansar <ansarbinkunhimarakar@gmail.com>
2026-09-10 16:31:12 +0530

    feat(home): weave official Vision & Mission framework into institutional homepage flow

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,37 +2,41 @@ import React from 'react';
 import HeroSection from '../components/home/HeroSection';
 import QuickAccessSection from '../components/home/QuickAccessSection';
 import AboutSection from '../components/home/AboutSection';
+import AcademicPillars from '../components/home/AcademicPillars';
+import InstitutionalHighlightsSection from '../components/home/InstitutionalHighlightsSection';
 import CoreServicesSection from '../components/home/CoreServicesSection';
 import LatestUpdatesSection from '../components/home/LatestUpdatesSection';
-import InstitutionalHighlightsSection from '../components/home/InstitutionalHighlightsSection';
 import ImportantResourcesSection from '../components/home/ImportantResourcesSection';
 import FinalCTA from '../components/home/FinalCTA';
 
 export const HomePage = () => {
   return (
     <div className="w-full bg-[#F7F8F5]">
-      {/* 1. CINEMATIC HERO */}
+      {/* 1. INSTITUTIONAL HERO */}
       <HeroSection />
 
       {/* 2. QUICK ACCESS GATEWAY */}
       <QuickAccessSection />
 
-      {/* 3. SHORT MISC INTRO */}
+      {/* 3. EDITORIAL ABOUT MISC INTRO */}
       <AboutSection />
 
-      {/* 4. CORE SERVICES / PILLARS */}
-      <CoreServicesSection />
+      {/* 4. VISION & MISSION FRAMEWORK */}
+      <AcademicPillars />
 
-      {/* 5. LATEST UPDATES & ANNOUNCEMENTS */}
-      <LatestUpdatesSection />
-
-      {/* 6. INSTITUTIONAL HIGHLIGHTS */}
+      {/* 5. THE MISC ECOSYSTEM & GOVERNANCE SCOPE */}
       <InstitutionalHighlightsSection />
 
-      {/* 7. IMPORTANT RESOURCES & PORTALS */}
+      {/* 6. ACADEMIC INFRASTRUCTURE & PILLARS */}
+      <CoreServicesSection />
+
+      {/* 7. NOTICES & ACADEMIC CIRCULARS */}
+      <LatestUpdatesSection />
+
+      {/* 8. ESSENTIAL RESOURCES & PORTALS */}
       <ImportantResourcesSection />
 
-      {/* 8. FINAL CLOSING CTA */}
+      {/* 9. FINAL CLOSING CTA */}
       <FinalCTA />
     </div>
   );
